0e60c74d7c2ab0e36054665f24c529c42bf06828
FredLin0421 <rederrrrr@gmail.com>
2019-11-27 18:27:51 -0800

    With constraint

--- Optimization/model.py
+++ Optimization/model.py
@@ -20,10 +20,17 @@ class Paraboloid(om.ExplicitComponent):
     """
 
     def setup(self):
-        self.add_input('length', val=np.zeros(2) )
-        self.add_input('kappa', val=np.zeros(3))
-        self.add_input('kb', val=np.zeros(3))
-        self.add_input('l22',val = 0.0)
+        self.add_input('length1', val=0.0)
+        self.add_input('length2', val=0.0)
+        self.add_input('length3', val=0.0)
+        self.add_input('length4', val=0.0)
+        self.add_input('kappa1', val=0.0)
+        self.add_input('kappa2', val=0.0)
+        self.add_input('kappa3', val=0.0)
+        self.add_input('kb1', val=0.0)
+        self.add_input('kb2', val=0.0)
+        self.add_input('kb3', val=0.0)
+        self.add_input('l22', val=0.0)
         self.add_output('f_xy', val=0.0)
 
         # Finite difference all partials.
@@ -35,19 +42,38 @@ class Paraboloid(om.ExplicitComponent):
 
         Minimum at: x = 6.6667; y = -7.3333
         """
-        length = inputs['length']
-        kappa = inputs['kappa']
-        kb = inputs['kb']
+        length1 = inputs['length1']
+        """length2 = inputs['length2']"""
+        length3 = inputs['length3']
+        length4 = inputs['length4']
+        """kappa1 = inputs['kappa1']"""
+        kappa2 = inputs['kappa2']
+        kappa3 = inputs['kappa3']
+        kb1 = inputs['kb1']
+        kb2 = inputs['kb2']
+        kb3 = inputs['kb3']
         l22 = inputs['l22']
-        def F_kinematics(inputs):
-           ptip = np.linalg.norm(length[0]*(cos((length[1]*(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]))/(kb[0] + kb[1] + kb[2]))*sin(((l22 - length[1])*(kappa[0]*kb[0] + kappa[1]*kb[1]))/(kb[0] + kb[1])) 
+        def F_kinematics_x(inputs):
+           """ptip = np.linalg.norm(length1*(cos((length3*(kappa1*kb1 + kappa[1]*kb[1] + kappa[2]*kb[2]))/(kb[0] + kb[1] + kb[2]))*sin(((l22 - length[1])*(kappa[0]*kb[0] + kappa[1]*kb[1]))/(kb[0] + kb[1])) 
             + sin((length[1]*(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]))/(kb[0] + kb[1] + kb[2]))*cos(((l22 - length[1])*(kappa[0]*kb[0] + kappa[1]*kb[1]))/(kb[0] + kb[1]))) 
             - ((cos((length[1]*(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]))/(kb[0] + kb[1] + kb[2])) - 1)*(kb[0] + kb[1] + kb[2]))/(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]) - 
             (cos((length[1]*(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]))/(kb[0] + kb[1] + kb[2]))*(cos(((l22 - length[1])*(kappa[0]*kb[0] + kappa[1]*kb[1]))/(kb[0] + kb[1])) - 1)*(kb[0] + kb[1]))/(kappa[0]*kb[0] + kappa[1]*kb[1]) 
             + (sin((length[1]*(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]))/(kb[0] + kb[1] + kb[2]))*sin(((l22 - length[1])*(kappa[0]*kb[0] + kappa[1]*kb[1]))/(kb[0] + kb[1]))*(kb[0] + kb[1]))/(kappa[0]*kb[0] + kappa[1]*kb[1]))
-           return ptip
-        
-        outputs['f_xy'] = np.linalg.norm(F_kinematics(inputs)-25)
+           """
+           kappa1 = 0
+           ptip_x = length1*(cos((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*sin(((l22-length3)*(kappa1*kb1 + kappa2*kb2))/(kb1 + kb2)) + sin((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*cos(((l22-length3)*(kappa1*kb1 + kappa2*kb2))/(kb1 + kb2))) - ((cos((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3)) - 1)*(kb1 + kb2 + kb3))/(kappa1*kb1 + kappa2*kb2 + kappa3*kb3) - (cos((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*(cos(((l22-length3)*(kappa1*kb1 + kappa2*kb2))/(kb1 + kb2)) - 1)*(kb1 + kb2))/(kappa1*kb1 + kappa2*kb2) + (sin((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*sin(((l22-length3)*(kappa1*kb1 + kappa2*kb2))/(kb1 + kb2))*(kb1 + kb2))/(kappa1*kb1 + kappa2*kb2)
+           return ptip_x
+        def F_kinematics_z(inputs):
+           """ptip = np.linalg.norm(length1*(cos((length3*(kappa1*kb1 + kappa[1]*kb[1] + kappa[2]*kb[2]))/(kb[0] + kb[1] + kb[2]))*sin(((l22 - length[1])*(kappa[0]*kb[0] + kappa[1]*kb[1]))/(kb[0] + kb[1])) 
+            + sin((length[1]*(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]))/(kb[0] + kb[1] + kb[2]))*cos(((l22 - length[1])*(kappa[0]*kb[0] + kappa[1]*kb[1]))/(kb[0] + kb[1]))) 
+            - ((cos((length[1]*(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]))/(kb[0] + kb[1] + kb[2])) - 1)*(kb[0] + kb[1] + kb[2]))/(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]) - 
+            (cos((length[1]*(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]))/(kb[0] + kb[1] + kb[2]))*(cos(((l22 - length[1])*(kappa[0]*kb[0] + kappa[1]*kb[1]))/(kb[0] + kb[1])) - 1)*(kb[0] + kb[1]))/(kappa[0]*kb[0] + kappa[1]*kb[1]) 
+            + (sin((length[1]*(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]))/(kb[0] + kb[1] + kb[2]))*sin(((l22 - length[1])*(kappa[0]*kb[0] + kappa[1]*kb[1]))/(kb[0] + kb[1]))*(kb[0] + kb[1]))/(kappa[0]*kb[0] + kappa[1]*kb[1]))
+           """
+           kappa1 = 0
+           ptip_z =  length4 + length1*(cos((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*cos(((l22 - length3)*(kappa1*kb1 + kappa2*kb2))/(kb1 + kb2)) - sin((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*sin(((l22 - length3)*(kappa1*kb1 + kappa2*kb2))/(kb1 + kb2))) + (sin((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*(kb1 + kb2 + kb3))/(kappa1*kb1 + kappa2*kb2 + kappa3*kb3) + (sin((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*(cos(((l22 - length3)*(kappa1*kb1 + kappa2*kb2))/(kb1 + kb2)) - 1)*(kb1 + kb2))/(kappa1*kb1 + kappa2*kb2) + (cos((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*sin(((l22 - length3)*(kappa1*kb1 + kappa2*kb2))/(kb1 + kb2))*(kb1 + kb2))/(kappa1*kb1 + kappa2*kb2)
+           return ptip_z
+        outputs['f_xy'] = np.linalg.norm(F_kinematics_x(inputs)-30+F_kinematics_z(inputs)-30)
         
         # "'outputs['f_xy'] = (x-3.0)**2 + x*y + (y+4.0)**2 - 3.0'"
 
@@ -56,16 +82,27 @@ if __name__ == "__main__":
 
     model = om.Group()
     ivc = om.IndepVarComp()
-    ivc.add_output('length', [1.0,1.0])
-    ivc.add_output('kappa', [1.0,1.0,1.0])
-    ivc.add_output('kb', [1.0,1.0,1.0])
-    ivc.add_output('l22', 1.0)
+    ivc.add_output('length1', 1.0)
+    """ivc.add_output('length2', 1.0)"""
+    ivc.add_output('length3', 1.0)
+    """ivc.add_output('kappa1', 1.0)"""
+    ivc.add_output('kappa2', 1.0)
+    ivc.add_output('kappa3', 1.0)
+    ivc.add_output('kb1', 1.0)
+    ivc.add_output('kb2', 1.0)
+    ivc.add_output('kb3', 1.0)
+    ivc.add_output('l22', 1.0 )
     model.add_subsystem('des_vars', ivc)
     model.add_subsystem('parab_comp', Paraboloid())
 
-    model.connect('des_vars.length', 'parab_comp.length')
-    model.connect('des_vars.kappa', 'parab_comp.kappa')
-    model.connect('des_vars.kb', 'parab_comp.kb')
+    model.connect('des_vars.length1', 'parab_comp.length1')
+    model.connect('des_vars.length3', 'parab_comp.length3')
+    """model.connect('des_vars.kappa1', 'parab_comp.kappa1')"""
+    model.connect('des_vars.kappa2', 'parab_comp.kappa2')
+    model.connect('des_vars.kappa3', 'parab_comp.kappa3')
+    model.connect('des_vars.kb1', 'parab_comp.kb1')
+    model.connect('des_vars.kb2', 'parab_comp.kb2')
+    model.connect('des_vars.kb3', 'parab_comp.kb3')
     model.connect('des_vars.l22', 'parab_comp.l22')
  
 
--- Optimization/optimizer.py
+++ Optimization/optimizer.py
@@ -16,50 +16,79 @@ prob = om.Problem()
 indeps = prob.model.add_subsystem('indeps', om.IndepVarComp())
 """indeps.add_output('x', 3.0)
 indeps.add_output('y', -4.0)"""
-indeps.add_output('length', np.array([1.0,1.0]))
-indeps.add_output('kappa', np.array([1.0,1.0,1.0]))
-indeps.add_output('kb', np.array([1.0,1.0,1.0]))
-indeps.add_output('l22', 1.0)
+indeps.add_output('length1', 1)
+"""ivc.add_output('length2', 1.0)"""
+indeps.add_output('length3', 1)
+indeps.add_output('length4', 1)
+"""indeps.add_output('kappa1', 1)"""
+indeps.add_output('kappa2', 1)
+indeps.add_output('kappa3', 1)
+indeps.add_output('kb1', 1)
+indeps.add_output('kb2', 1)
+indeps.add_output('kb3', 1)
+indeps.add_output('l22', 1)
 
 prob.model.add_subsystem('parab', Paraboloid())
 
 # define the component whose output will be constrained
-"""prob.model.add_subsystem('const', om.ExecComp('g = length[0]*(cos((length[1]*(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]))/(kb[0] + kb[1] + kb[2]))*sin(((l22 - length[1])*(kappa[0]*kb[0] + kappa[1]*kb[1]))/(kb[0] + kb[1])) + sin((length[1]*(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]))/(kb[0] + kb[1] + kb[2]))*cos(((l22 - length[1])*(kappa[0]*kb[0] + kappa[1]*kb[1]))/(kb[0] + kb[1]))) - ((cos((length[1]*(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]))/(kb[0] + kb[1] + kb[2])) - 1)*(kb[0] + kb[1] + kb[2]))/(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]) - (cos((length[1]*(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]))/(kb[0] + kb[1] + kb[2]))*(cos(((l22 - length[1])*(kappa[0]*kb[0] + kappa[1]*kb[1]))/(kb[0] + kb[1])) - 1)*(kb[0] + kb[1]))/(kappa[0]*kb[0] + kappa[1]*kb[1]) + (sin((length[1]*(kappa[0]*kb[0] + kappa[1]*kb[1] + kappa[2]*kb[2]))/(kb[0] + kb[1] + kb[2]))*sin(((l22 - length[1])*(kappa[0]*kb[0] + kappa[1]*kb[1]))/(kb[0] + kb[1]))*(kb[0] + kb[1]))/(kappa[0]*kb[0] + kappa[1]*kb[1])'))
-"""
+prob.model.add_subsystem('const1', om.ExecComp('g = length1*(cos((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*sin(((l22 - length3)*(kappa1*kb1 + kappa2*kb2))/(kb1 + kb2)) + sin((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*cos(((l22 - length3)*(kappa1*kb1 + kappa2*kb2))/(kb1 + kb2))) - ((cos((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3)) - 1)*(kb1 + kb2 + kb3))/(kappa1*kb1 + kappa2*kb2 + kappa3*kb3) - (cos((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*(cos(((l22 - length3)*(kappa1*kb1 + kappa2*kb2))/(kb1 + kb2)) - 1)*(kb1 + kb2))/(kappa1*kb1 + kappa2*kb2) + (sin((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*sin(((l22 - length3)*(kappa1*kb1 + kappa2*kb2))/(kb1 + kb2))*(kb1 + kb2))/(kappa1*kb1 + kappa2*kb2)'))
+prob.model.add_subsystem('const2', om.ExecComp('g =  length4 + length1*(cos((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*cos(((l22 - length3)*(kappa1*kb1 + kappa2*kb2))/(kb1 + kb2)) - sin((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*sin(((l22 - length3)*(kappa1*kb1 + kappa2*kb2))/(kb1 + kb2))) + (sin((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*(kb1 + kb2 + kb3))/(kappa1*kb1 + kappa2*kb2 + kappa3*kb3) + (sin((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*(cos(((l22 - length3)*(kappa1*kb1 + kappa2*kb2))/(kb1 + kb2)) - 1)*(kb1 + kb2))/(kappa1*kb1 + kappa2*kb2) + (cos((length3*(kappa1*kb1 + kappa2*kb2 + kappa3*kb3))/(kb1 + kb2 + kb3))*sin(((l22 - length3)*(kappa1*kb1 + kappa2*kb2))/(kb1 + kb2))*(kb1 + kb2))/(kappa1*kb1 + kappa2*kb2)'))
+prob.model.add_subsystem('const3', om.ExecComp('g = l22 - length3'))
 '''prob.model.connect('indeps.x', ['parab.x', 'const.x'])
 prob.model.connect('indeps.y', ['parab.y', 'const.y'])'''
-"""
-prob.model.connect('indeps.length', ['parab.length','const.length'])
-prob.model.connect('indeps.kappa', ['parab.kappa', 'const.kappa'])
-prob.model.connect('indeps.kb', ['parab.kb', 'const.kb'])"""
-prob.model.connect('indeps.l22', 'parab.l22')
-prob.model.connect('indeps.length', 'parab.length')
-prob.model.connect('indeps.kappa', 'parab.kappa')
-prob.model.connect('indeps.kb', 'parab.kb')
+
+prob.model.connect('indeps.l22', ['parab.l22','const1.l22','const2.l22','const3.l22'])
+prob.model.connect('indeps.length1', ['parab.length1','const1.length1','const2.length1'])
+prob.model.connect('indeps.length3', ['parab.length3','const1.length3','const2.length3','const3.length3'])
+prob.model.connect('indeps.length4', ['parab.length4','const2.length4'])
+"""prob.model.connect('indeps.kappa1', ['parab.kappa1','const1.kappa1','const2.kappa1'])"""
+prob.model.connect('indeps.kappa2', ['parab.kappa2','const1.kappa2','const2.kappa2'])
+prob.model.connect('indeps.kappa3', ['parab.kappa3','const1.kappa3','const2.kappa3'])
+prob.model.connect('indeps.kb1', ['parab.kb1','const1.kb1','const2.kb1'])
+prob.model.connect('indeps.kb2', ['parab.kb2','const1.kb2','const2.kb2'])
+prob.model.connect('indeps.kb3', ['parab.kb3','const1.kb3','const2.kb3'])
 
 # setup the optimization
 prob.driver = om.ScipyOptimizeDriver()
 prob.driver.options['optimizer'] = 'SLSQP'
+"""prob.driver.options['maxiter'] = 500"""
 
-prob.model.add_design_var('indeps.length', lower=0, upper=50)
-prob.model.add_design_var('indeps.kappa', lower=0, upper=50)
-prob.model.add_design_var('indeps.kb', lower=0, upper=50)
-prob.model.add_design_var('indeps.l22', lower=0, upper=50)
+prob.model.add_design_var('indeps.length1', lower=1, upper=50)
+prob.model.add_design_var('indeps.length3', lower=1, upper=50)
+prob.model.add_design_var('indeps.length4', lower=1, upper=50)
+"""prob.model.add_design_var('indeps.kappa1', lower=0, upper=10)"""
+prob.model.add_design_var('indeps.kappa2', lower=0, upper=10)
+prob.model.add_design_var('indeps.kappa3', lower=0, upper=10)
+prob.model.add_design_var('indeps.kb1', lower=1, upper=50)
+prob.model.add_design_var('indeps.kb2', lower=1, upper=50)
+prob.model.add_design_var('indeps.kb3', lower=1, upper=50)
+prob.model.add_design_var('indeps.l22', lower=1, upper=50)
 prob.model.add_objective('parab.f_xy')
 
 # to add the constraint to the model
-"""prob.model.add_constraint('const.g', lower=0, upper=2.)"""
-# prob.model.add_constraint('const.g', equals=0.)
+"""
+prob.model.add_constraint('const1.g', lower=29.5, upper=30.0)
+prob.model.add_constraint('const2.g', lower=29.5, upper=30.0)"""
+prob.model.add_constraint('const1.g', equals=30)
+prob.model.add_constraint('const2.g', equals=30)
+prob.model.add_constraint('const3.g', lower=1, upper=30)
 
 prob.setup()
 prob.run_driver()
 
 # minimum value
-print(prob['parab.f_xy'])
+#print(prob['parab.f_xy'])
 
 # location of the minimum
-print(prob['indeps.length'])
-print(prob['indeps.kappa'])
-print(prob['indeps.kb'])
-print(prob['indeps.l22'])
+print('Length1 =',prob['indeps.length1'])
+print('Length2 =',prob['indeps.l22']-prob['indeps.length3'])
+print('Length3 =',prob['indeps.length3'])
+print('Length4 =',prob['indeps.length4'])
+print('kappa1 = [0]')
+print('kappa2 = ',prob['indeps.kappa2'])
+print('kappa3 = ',prob['indeps.kappa3'])
+print('kb1 = ',prob['indeps.kb1'])
+print('kb2 = ',prob['indeps.kb2'])
+print('kb3 = ',prob['indeps.kb3'])
+print('l22 = ',prob['indeps.l22'])
 
